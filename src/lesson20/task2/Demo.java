12161d28e04d1ccddedf478b850c61acb33a5d09
hladon <41907863+hladon@users.noreply.github.com>
2018-10-31 16:01:56 +0200

    Update Demo.java

--- src/lesson20/task2/Demo.java
+++ src/lesson20/task2/Demo.java
@@ -5,11 +5,19 @@ import java.util.Date;
 public class Demo {
     public static void main(String[] args) {
         Controller test=new Controller();
-
-        Transaction tr1=new Transaction(1,"Kiev",10,"test1",TransactionType.INCOME,new Date(28/10/2018));
-        Transaction tr2=new Transaction(1,"Kiev",20,"test1",TransactionType.INCOME,new Date(28/10/2018));
-        Transaction tr3=new Transaction(1,"Odessa",10,"test1",TransactionType.INCOME,new Date(28/10/2018));
-        Transaction tr4=new Transaction(1,"Odessa",20,"test1",TransactionType.INCOME,new Date(28/10/2018));
+        Transaction tr1=null;
+        Transaction tr2=null;
+        Transaction tr3=null;
+        Transaction tr4=null;
+        
+        try{
+         tr1=new Transaction(1,"Kiev",10,"test1",TransactionType.INCOME,new Date(28/10/2018));
+         tr2=new Transaction(1,"Kiev",20,"test1",TransactionType.INCOME,new Date(28/10/2018));
+         tr3=new Transaction(1,"Odessa",10,"test1",TransactionType.INCOME,new Date(28/10/2018));
+         tr4=new Transaction(1,"Odessa",20,"test1",TransactionType.INCOME,new Date(28/10/2018));
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
 
         try{
             test.save(tr1);
