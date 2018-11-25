b5056a259588b613e769e29dc32efd345ff514cc
Vitaliy <hladon@meta.ua>
2018-11-25 22:35:52 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/lesson20/task2/Demo.java
    #	src/lesson20/task2/TransactionDAO.java

--- src/lesson32/Solution.java
+++ src/lesson32/Solution.java
@@ -1,31 +1,30 @@
 package lesson32;
 
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Solution {
     public static int readNumbers() {
-        Scanner scanner = new Scanner(System.in);
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(reader);
         int attempt = 3;
         int numbers = 10;
         int sum = 0;
         int number = 0;
-        while (scanner.hasNext()) {
+        while (numbers != 0) {
             try {
-                number = Integer.parseInt(scanner.nextLine());
+                number = Integer.parseInt(bufferedReader.readLine());
                 sum += number;
                 numbers--;
             } catch (Exception e) {
+                if (attempt == 0) {
+                    System.out.println("Your numbers are wrong. Numbers of attempts exceeded");
+                    break;
+                }
                 attempt--;
                 System.out.println("Your numbers are wrong. You have " + attempt + " attempta to try again");
             }
-            if (attempt == 0) {
-                System.out.println("Your numbers are wrong. Numbers of attempts exceeded");
-                return 0;
-            }
-
-            if (numbers == 0)
-                break;
 
         }
 
